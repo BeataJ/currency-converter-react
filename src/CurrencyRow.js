aa91054fce66106cf9df8073289ae6f101779636
beata <beata@emc2wave.com>
2020-02-24 12:11:01 -0800

    add input select option

--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
 export default function CurrencyRow() {
-  return <div>Currency Row</div>;
+  return (
+    <div>
+      <input type='number' />
+      <select>
+        <option value='Hi'>Hi</option>
+      </select>
+    </div>
+  );
 }
